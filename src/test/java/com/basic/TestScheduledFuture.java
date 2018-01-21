d94074b7c79f6c07ecb567091f63745ae8965480
oyxiaogua <mycg8199mail@163.com>
2018-01-21 14:39:56 +0800

    测试future get阻塞

--- src/test/java/com/basic/TestScheduledFuture.java
+++ src/test/java/com/basic/TestScheduledFuture.java
@@ -1,11 +1,15 @@
 package com.basic;
 
 import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -35,5 +39,43 @@ public class TestScheduledFuture {
 
 		TimeUnit.MINUTES.sleep(1);
 		service.shutdown();
+	}
+
+	@Test(expected=TimeoutException.class)
+	public void testFutureGetBlock() throws Exception {
+		ThreadPoolExecutor executorService = new ThreadPoolExecutor(1, 1, 1L, TimeUnit.MINUTES,
+				new ArrayBlockingQueue<Runnable>(1), new ThreadPoolExecutor.DiscardPolicy());
+		Future<?> futureOne = executorService.submit(new Runnable() {
+			public void run() {
+				log.info("start runable one");
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					log.error("futureOne interrupted", e);
+				}
+			}
+		});
+
+		Future<?> futureTwo = executorService.submit(new Runnable() {
+			public void run() {
+				log.info("start runable two");
+			}
+		});
+
+		Future<?> futureThree = null;
+		try {
+			futureThree = executorService.submit(new Runnable() {
+				public void run() {
+					log.info("start runable three");
+				}
+			});
+		} catch (Exception e) {
+			log.error("futureThree error", e);
+		}
+		log.info("task on={} ", futureOne.get());
+		log.info("task two={}", futureTwo.get());
+		//log.info("task three={} ", (futureThree == null ? null : futureThree.get()));
+		log.info("task three={} ", (futureThree == null ? null : futureThree.get(3,TimeUnit.SECONDS)));
+		executorService.shutdown();
 	}
 }
