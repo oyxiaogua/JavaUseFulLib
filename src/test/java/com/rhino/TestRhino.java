36bcc65fbcc5b8cff4253b55dc94d78ce58720ea
oyxiaogua <mycg8199mail@163.com>
2018-01-28 11:48:23 +0800

    测试rhino

--- src/test/java/com/rhino/TestRhino.java
+++ src/test/java/com/rhino/TestRhino.java
@@ -1,7 +1,15 @@
 package com.rhino;
 
+import java.io.File;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import org.slf4j.Logger;
@@ -26,6 +34,50 @@ public class TestRhino {
 		scope.put("myUtil", scope, myUtil);
 		jsStr = "log.info(myUtil.trim(\"   测试   \"));";
 		ctx.evaluateString(scope, jsStr, null, 0, null);
+
+		jsStr = "9*(1+2)";
+		Object objRtn = ctx.evaluateString(scope, jsStr, null, 0, null);
+		double dbRtn = Context.toNumber(objRtn);
+		log.info("rtn={}", dbRtn);
+
+		jsStr = "function helloFunc(v){ return v;}";
+		ctx.evaluateString(scope, jsStr, "helloFunc", 0, null);
+		Function helloFunc = (Function) scope.get("helloFunc", scope);
+		Object call = helloFunc.call(Context.getCurrentContext(), scope, helloFunc, new Object[] { "test测试" });
+		log.info("rtn={}", call.toString());
+
+		// 加载jquery
+		jsStr = "var print = function(v){log.debug(\"v\");return v;}";
+		ctx.setOptimizationLevel(-1);
+		ctx.evaluateString(scope, jsStr, "print", 0, null);
+		String jqueryStr = FileUtils.readFileToString(new File("src/test/resources/env.rhino.1.2.js"));
+		ctx.evaluateString(scope, jqueryStr, "env.rhino.1.2.js", 0, null);
+
+		jqueryStr = FileUtils.readFileToString(new File("src/test/resources/jquery.min.js"));
+		ctx.evaluateString(scope, jqueryStr, "jquery.min.js", 0, null);
+
+		jsStr = "function getJsonStr(){var aObj={\"key_1\":\"测试\",subArr:[\"测试2\",\"test3\"]}; return JSON.stringify(aObj);}";
+		ctx.evaluateString(scope, jsStr, "getJsonStr", 0, null);
+		helloFunc = (Function) scope.get("getJsonStr", scope);
+		call = helloFunc.call(Context.getCurrentContext(), scope, helloFunc, new Object[] {});
+		log.info("rtn={}", call.toString());
 		Context.exit();
+	}
+
+	@Test
+	public void testNashornRunSimpleJsCode() throws Exception {
+		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+		ScriptEngine nashorn = scriptEngineManager.getEngineByName("nashorn");
+		String jsStr = "function hello(v){return \"hello,\"+v}";
+		nashorn.eval(jsStr);
+		Invocable invocable = (Invocable) nashorn;
+		Object result = invocable.invokeFunction("hello", new Object[] { "测试" });
+		log.info("rtn={}", result.toString());
+
+		jsStr = "var stringUtils = Java.type(\"org.apache.commons.lang3.StringUtils\");function myTrim(v){return stringUtils.trim(v);}";
+		nashorn.eval(jsStr);
+		invocable = (Invocable) nashorn;
+		result = invocable.invokeFunction("myTrim", new Object[] { "   测试   " });
+		log.info("rtn={}", result.toString());
 	}
 }
