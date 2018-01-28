8fb25681d7dcea7cdadf8aa88e932c4df6bbeef5
oyxiaogua <mycg8199mail@163.com>
2018-01-28 14:32:52 +0800

    测试java尾递归

---
+++ src/test/java/com/basic/TestJava8.java
@@ -0,0 +1,100 @@
+package com.basic;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestJava8 {
+	private static final Logger log = LoggerFactory.getLogger(TestJava8.class);
+
+	@Test
+	public void testFibonacciTailRec() {
+		long start = System.currentTimeMillis();
+		log.info("rtn={},cost time={}", factorialTailRecu(10), (System.currentTimeMillis() - start));
+
+	}
+
+	@Test
+	public void testFibonacciMemoOpt() {
+		long start = System.currentTimeMillis();
+		log.info("rtn={},cost time={}", fibonacciMemo(10), (System.currentTimeMillis() - start));
+	}
+
+	public static long factorialTailRecu(final long number) {
+		return factorialTailRecursion(1, number).invoke();
+	}
+
+	/**
+	 * 阶乘计算 -- 使用尾递归接口完成
+	 * 
+	 * @param factorial
+	 *            当前递归栈的结果值
+	 * @param number
+	 *            下一个递归需要计算的值
+	 * @return 尾递归接口,调用invoke启动及早求值获得结果
+	 */
+	public static TailRecursion<Long> factorialTailRecursion(final long factorial, final long number) {
+		if (number == 1) {
+			return TailInvoke.done(factorial);
+		} else {
+			return TailInvoke.call(() -> factorialTailRecursion(factorial + number, number - 1));
+		}
+	}
+
+	/**
+	 * 使用统一封装的备忘录模式 对外开放的方法,在内部执行具体的斐波那契策略
+	 * {@link #fibonacciCallMemo(Function, Integer)}
+	 * 
+	 * @param n
+	 *            第n个斐波那契数
+	 * @return 第n个斐波那契数
+	 */
+	public static long fibonacciMemo(int n) {
+		return callMemo(TestJava8::fibonacciCallMemo, n);
+	}
+
+	/**
+	 * 私有方法,服务于{@link #fibonacciMemo(int)} ,内部实现为斐波那契算法策略
+	 * 
+	 * @param fib
+	 *            斐波那契算法策略自身,用于递归调用, 在{@link #callMemo(BiFunction, Object)}
+	 *            中通过传入this来实例这个策略
+	 * @param n
+	 *            第n个斐波那契数
+	 * @return 第n个斐波那契数
+	 */
+	private static long fibonacciCallMemo(Function<Integer, Long> fib, Integer n) {
+		if (n == 0 || n == 1)
+			return 1;
+		return fib.apply(n - 1) + fib.apply(n - 2);
+	}
+
+	/**
+	 * 备忘录模式 函数封装
+	 * 
+	 * @param function
+	 *            递归策略算法
+	 * @param input
+	 *            输入值
+	 * @param <T>
+	 *            输出值类型
+	 * @param <R>
+	 *            返回值类型
+	 * @return 将输入值输入递归策略算法，计算出的最终结果
+	 */
+	public static <T, R> R callMemo(final BiFunction<Function<T, R>, T, R> function, final T input) {
+		Function<T, R> memo = new Function<T, R>() {
+			private final Map<T, R> cache = new HashMap<>(64);
+
+			public R apply(final T input) {
+				return cache.computeIfAbsent(input, key -> function.apply(this, key));
+			}
+		};
+		return memo.apply(input);
+	}
+}
